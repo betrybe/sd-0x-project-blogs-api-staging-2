32d8ece87905cdbd4deb812bf41af265aa9113ad
Cristiano <contato@cristianoprogramador.com>
2021-01-24 08:10:03 -0300

    Configura arquico config.js

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+  use_env_variable: true,
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+};
