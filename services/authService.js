677d389087cf4d34255eadf83f6080a4425d50ad
Cristiano <contato@cristianoprogramador.com>
2021-01-24 08:26:03 -0300

    Passa o Id e o name para o token[

--- services/authService.js
+++ services/authService.js
@@ -1,15 +1,14 @@
-const AuthResponse = require('../models/auth/authResponse');
 const jwt = require('jsonwebtoken');
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "VF^6e7k2^5N@"
 
 const authenticateUser = (login, password) => {
     if (login === 'abc' && password === '123')
-        return new AuthResponse(true, generateJWT(1));
+        return { token: generateJWT("", "") }
     return new AuthResponse(false, 'Login ou senha inválidos');
 }
 
-const generateJWT = (userId) => {
-    return jwt.sign({ userId }, TOKEN_SECRET, { expiresIn: '1800s' })
+const generateJWT = (userId, name) => {
+    return jwt.sign({ userId, name }, TOKEN_SECRET, { expiresIn: '1800s' })
 }
 
 const tokenIsValid = (token) => {
