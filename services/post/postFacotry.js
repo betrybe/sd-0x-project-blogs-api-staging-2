c078339b8cf00820b3e80b005df93aabde69c5a1
Cristiano <contato@cristianoprogramador.com>
2021-01-26 17:59:15 -0300

    Eslint

--- services/post/postFacotry.js
+++ services/post/postFacotry.js
@@ -10,7 +10,7 @@ const generateInstance = () => {
     getById: postService.getById(postRepository, User),
     deletePost: postService.deletePost(postRepository),
     search: postService.search(postRepository),
-    edit: postService.edit(postRepository)
+    edit: postService.edit(postRepository),
   };
 };
 
